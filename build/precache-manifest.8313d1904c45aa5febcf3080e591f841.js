5fdc0daad265e54ce39dc1a55e0c28c8eab018ac
Francisco Alvarado <102932362+falvarado7@users.noreply.github.com>
2023-12-29 00:24:38 -0600

    Add files via upload

---
+++ build/precache-manifest.8313d1904c45aa5febcf3080e591f841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df718a9513416a2e9e62b508df57705",
+    "url": "/index.html"
+  },
+  {
+    "revision": "031a4df23507ba28628b",
+    "url": "/static/css/main.3d9e1829.chunk.css"
+  },
+  {
+    "revision": "dbf19fd96ea7ed36de7a",
+    "url": "/static/js/2.e1e0ccdf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e1e0ccdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031a4df23507ba28628b",
+    "url": "/static/js/main.6104e698.chunk.js"
+  },
+  {
+    "revision": "c8e5fd3b141acc9e29e7",
+    "url": "/static/js/runtime-main.e51d1157.js"
+  }
+]);
